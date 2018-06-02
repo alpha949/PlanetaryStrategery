876f8c6538e1ab4701a1bdb881c4fd514af4de87
Zakiah <zschaefle1@gmail.com>
2018-06-01 19:10:28 -0700

    Resource Sharing
    Planets in a network should cooperate to share resources with eachother
    so that no individual planet is lacking. I implemented other things that
    I needed as I though they would best be implemented. If you don't like
    something change it, but don't complain when my code stops working.
    
    I recognize resource sharing is inefficient right now, it'll be better
    later hopefully.

--- core/src/com/ue/ps/buildings/SpaceFactory.java
+++ core/src/com/ue/ps/buildings/SpaceFactory.java
@@ -1,24 +1,45 @@
 package com.ue.ps.buildings;
 
 import com.ue.ps.Planet;
+import com.ue.ps.ships.Ship;
+import com.ue.ps.ships.ShipType;
 import com.ue.ps.ui.Images;
 
 public class SpaceFactory extends Building{
 
+	public ShipType buildingShip;
+	
+	public int buildProgress, consumption, efficiency;
+	
 	public SpaceFactory() {
 		super(Images.spaceFactory);
 		this.id = 's';
 		this.health = 50;
 		this.resourceCost = 5; //should cost more but build ships cheaper/faster
-		
+		this.buildProgress = 0;
 		this.isSpace = true;
 		this.type = Building.Types.spaceFactory;
+		this.consumption = 4;
+		this.efficiency = 4;
 	}
 
 	@Override
 	public void update(Planet p) {
-		// TODO Auto-generated method stub
+		int intake = this.getConsumption(p) - (this.getConsumption(p) % efficiency);
+		p.resource -= intake;
+		this.buildProgress += intake;
 		
+		if((this.buildProgress == this.buildingShip.getStat(2) + p.owner.faction.getMod(buildingShip, 2)) && (buildingShip != null)){
+			Ship.spawnShip(p.owner, p, buildingShip, 0);
+			buildProgress = 0;
+			buildingShip = null;
+		}
+	}
+	
+	public int getConsumption(Planet p){
+		int a = this.buildingShip.getStat(2) + p.owner.faction.getMod(buildingShip, 2) - buildProgress;
+		a *= efficiency;
+		return consumption <= a ? consumption : a;
 	}
 
 }
